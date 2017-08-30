a29022afe20bd61ff156d0f9793d8806d3509366
Andrews Medina <andrewsmedina@gmail.com>
2017-08-30 00:13:16 -0300

    add csv importer
    related to #1

--- importer/csv.go
+++ importer/csv.go
@@ -1,7 +1,28 @@
 package importer
 
+import (
+	"encoding/csv"
+	"io"
+
+	"github.com/andrewsmedina/tabular"
+)
+
 // CSV represents a csv importer
 type CSV struct{}
 
 // Import imports a csv into a tabular.Table
-func (c *CSV) Import() {}
+func (c *CSV) Import(reader io.Reader) (*tabular.Table, error) {
+	r := csv.NewReader(reader)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	table := &tabular.Table{}
+
+	for _, row := range records {
+		table.Append(row)
+	}
+	return table, nil
+}
--- importer/csv_test.go
+++ importer/csv_test.go
@@ -1,1 +1,18 @@
 package importer
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCSVImporter(t *testing.T) {
+	data := `first_name,last_name,username
+Andrews,Medina,andrewsmedina`
+	csvImpoter := &CSV{}
+
+	_, err := csvImpoter.Import(strings.NewReader(data))
+
+	if err != nil {
+		t.Fatalf("expect nil, got %q", err)
+	}
+}
