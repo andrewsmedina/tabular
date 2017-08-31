71361db8363d2ea12a64108bd969ce78e58265bf
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2017-08-31 00:08:56 -0300

    table: set fields method.

--- table.go
+++ table.go
@@ -1,5 +1,10 @@
 package tabular
 
+import (
+	"reflect"
+	"errors"
+)
+
 // Type represents a field type
 type Type string
 
@@ -16,15 +21,31 @@ type Field struct {
 
 // Table represents a table
 type Table struct {
-	Fields []Field
-	Data   []interface{}
+	fields []Field
+	data   []interface{}
+}
+
+// SetFields add fields and respectives types in table
+func (t *Table) SetFields(data interface{}) error {
+	s := reflect.ValueOf(data)
+    if s.Kind() != reflect.Slice {
+        return errors.New("data given a non-slice type")
+	}
+	for i:=0; i<s.Len(); i++ {
+		field := Field{
+			Name: s.Index(i).String(),
+			Type: String,
+		}
+		t.fields = append(t.fields, field)
+	}
+	return nil
 }
 
 // Append adds a row to a table data
 func (t *Table) Append(row interface{}) {
-	if t.Data == nil {
-		t.Data = []interface{}{}
+	if t.data == nil {
+		t.data = []interface{}{}
 	}
 
-	t.Data = append(t.Data, row)
+	t.data = append(t.data, row)
 }
--- table_test.go
+++ table_test.go
@@ -6,14 +6,31 @@ import (
 
 func TestTable(t *testing.T) {
 	table := Table{
-		Fields: []Field{
+		fields: []Field{
 			{Name: "name", Type: String},
 		},
 	}
 	table.Append(map[string]string{"name": "Andrews"})
 	table.Append(map[string]string{"name": "Tarsis"})
 
-	if len(table.Data) != 2 {
-		t.Fatalf("table data length should be 2, got %q", len(table.Data))
+	if len(table.data) != 2 {
+		t.Fatalf("table data length should be 2, got %q", len(table.data))
+	}
+}
+
+func TestFields(t *testing.T) {
+	table := Table{}
+	table.SetFields([]string{"a", "b"})
+	expected := []Field{
+		{Name: "a", Type: String},
+		{Name: "b", Type: String},
+	}
+	for i, f := range expected {
+		if f.Name != table.fields[i].Name {
+			t.Fatalf("field name is wrong. Expect %s, got %s", f.Name, table.fields[i].Name)
+		}
+		if f.Type != table.fields[i].Type {
+			t.Fatalf("field type is wrong. Expect %s, got %s", f.Type, table.fields[i].Type)
+		}
 	}
 }
